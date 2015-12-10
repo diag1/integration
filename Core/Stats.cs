a12db3cd313a40175aa152612948b3bc6d271c20
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2015-12-10 10:02:57 +0100

    Added bin

---
+++ Core/Stats.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace calendar
+{
+	/// <summary>
+	/// Fachada is an interface that contains methods for getting statistics.
+	/// </summary>
+	public interface Fachada
+	{	
+		String getDistTot();
+		String getDistDay(DateTime fecha);
+		String getVelMedTot();
+		String getVelMedDay(DateTime fecha);
+		String getNumStpsTot();
+		String getNumStpsDay(DateTime fecha);
+		String getNumHourTot();
+		String getNumHourDay(DateTime fecha);
+	}
+}
+
---
+++ Core/Transformer.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace integration
+{
+	public interface Transformer
+	{
+	}
+}
+
